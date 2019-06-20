1cf89a1d55f126698426ee4ed95040e335d34aba
jeffreytram <jeffrey_tram@yahoo.com>
2019-06-20 15:07:27 -0400

    add hints for textfields remove need for set button add/delete/calculate button pressed, save current values add note if grade percent goes over 100% add about/description section accounts for weight totals over 100 (extra credit)

--- script.js
+++ script.js
@@ -3,28 +3,103 @@ let gradeList = {
     addGrade: function(){
         this.grades.push({
             title: null,
-            percent: 0,
-            grade: 0
+            percent: null,
+            grade: null
         });
     },
-    setGrade: function(title, percent, grade, position){
-        this.grades[position].title = title;
-        this.grades[position].percent = percent;
-        this.grades[position].grade = grade;
+    setAllGrades: function(){
+        this.grades.forEach(function(grade, position){
+            grade.title = document.getElementById(position + ".1").value;
+            grade.percent = parseFloat(document.getElementById(position + ".2").value);
+            grade.grade = parseFloat(document.getElementById(position + ".3").value);
+        });
+    },
+    deleteGrade: function(position){
+        this.grades.splice(position,1);
+    },
+    validInput: function(percent, grade){
+        if(percent>=0 && !isNaN(grade)){
+            return true;
+        }
+        return false;
+    },
+    calculateCurrentGrade: function(){
+        let currentGrade = 0;
+        let completedPercent = 0;
+        let totalPercent = 0;
+        let forecast = false;
+        this.grades.forEach(function(grade){
+            if(this.validInput(grade.percent, grade.grade) && grade.grade>=0){
+                currentGrade += grade.percent * grade.grade;
+                completedPercent += grade.percent;
+            }
+            else if(grade.grade<0){
+                forecast = true;
+            }
+            totalPercent += grade.percent;
+        }, this);
+        if(totalPercent >1){
+            view.displayOverHundredWarning();
+        }
+        if(completedPercent === 0){
+            return 0;
+        }
+        if(forecast){
+            if(completedPercent>1){
+                completedPercent = 1;
+            }
+            return currentGrade/completedPercent;
+        }
+        return currentGrade
+    },
+    calculateLowestGrade: function(){
+        let lowestGrade = 0;
+        this.grades.forEach(function(grade){
+            if(this.validInput(grade.percent, grade.grade)){
+                if(grade.grade<0){
+                    lowestGrade += grade.percent *0;
+                }
+                else{
+                    lowestGrade += grade.percent * grade.grade;
+                }
+            }
+        }, this);
+        return lowestGrade;
+    },
+    calculateHighestGrade: function(){
+        let highestGrade = 0;
+        this.grades.forEach(function(grade){
+            if(this.validInput(grade.percent, grade.grade)){
+                if(grade.grade<0){
+                    highestGrade += grade.percent *100;
+                }
+                else{
+                    highestGrade += grade.percent * grade.grade;
+                }
+            }
+        }, this);
+        return highestGrade;
     }
 };
 //button handlers
 let handlers = {
     addGrade: function(){
+        gradeList.setAllGrades();
         gradeList.addGrade();
         view.displayGrades();
     },
-    setGrade: function(position){
-        let title = document.getElementById(position + ".1").value;
-        let percent = parseInt(document.getElementById(position + ".2").value);
-        let grade = parseInt(document.getElementById(position + ".3").value);
-        gradeList.setGrade(title, percent, grade, position);
-
+    deleteGrade: function(position){
+        gradeList.setAllGrades();
+        gradeList.deleteGrade(position);
+        view.displayGrades();
+    },
+    calculateCurrentGrade: function(){
+        view.clearCurrentGradeDisplay();
+        gradeList.setAllGrades();
+        let currentGrade = gradeList.calculateCurrentGrade();
+        let lowestGrade = gradeList.calculateLowestGrade();
+        let highestGrade = gradeList.calculateHighestGrade();
+        view.displayCurrentGrade(currentGrade,lowestGrade,highestGrade);
     }
 };
 
@@ -40,39 +115,55 @@ let view = {
             let gradeInputTitle = document.createElement("input");
             gradeInputTitle.value = grade.title;
             gradeInputTitle.type = "text";
+            gradeInputTitle.placeholder = "Grade Name "+(position+1);
             gradeInputTitle.id = position + ".1";
 
             let gradeInputPercent = document.createElement("input");
             gradeInputPercent.value = grade.percent;
             gradeInputPercent.type = "number";
+            gradeInputPercent.placeholder = "Grade Weight "+(position+1);
             gradeInputPercent.id = position + ".2";
 
             let gradeInputGrade = document.createElement("input");
              gradeInputGrade.value = grade.grade;
             gradeInputGrade.type = "number";
+            gradeInputGrade.placeholder = "Grade Earned "+(position+1);
             gradeInputGrade.id = position + ".3";
 
-            let gradeSetButton = document.createElement("button");
-            gradeSetButton.textContent = "Set";
-            gradeSetButton.className = "gradeSetButton";
-            gradeSetButton.id = position;
+            let gradeDeleteButton = document.createElement("button");
+            gradeDeleteButton.textContent = "Delete";
+            gradeDeleteButton.className = "gradeDeleteButton";
+            gradeDeleteButton.id = position;
 
             gradesDiv.appendChild(gradeInputTitle);
             gradesDiv.appendChild(gradeInputPercent);
             gradesDiv.appendChild(gradeInputGrade);
-            gradesDiv.appendChild(gradeSetButton);
+            gradesDiv.appendChild(gradeDeleteButton);
             gradesDiv.id = position;
 
             gradesUl.appendChild(gradesDiv);
         });
     },
     setUpEventListeners: function(){
+        //add addgrade event listener
         let gradesUl = document.querySelector("ul");
         gradesUl.addEventListener("click",function(event){
-            if(event.target.className === "gradeSetButton"){
-                handlers.setGrade(event.target.parentNode.id);
+            if(event.target.className === "gradeDeleteButton"){
+                handlers.deleteGrade(event.target.parentNode.id);
             }
         });
+    },
+    displayCurrentGrade: function(cur,low,high){
+        let gradesP = document.getElementById("calculations");
+        gradesP.innerHTML += "Current Grade: " + cur +"%<br/>Lowest Possible Grade: "+ low+"%<br/>Highest Possible Grade: "+high+"%";
+    },
+    displayOverHundredWarning: function(){
+        let gradesP = document.getElementById("calculations");
+        gradesP.innerHTML += "Note: The grade weight total exceeds 100%. Your class should include extra credit<br/><br/>";
+    },
+    clearCurrentGradeDisplay: function(){
+        let gradesP = document.getElementById("calculations");
+        gradesP.innerHTML = "";
     }
 };
 
