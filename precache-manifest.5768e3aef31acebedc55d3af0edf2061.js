b5676c731552b2392b30a0aa958167cd2ef89c82
jeffreytram <jeffrey_tram@yahoo.com>
2020-07-15 01:49:18 -0400

    Updates

---
+++ precache-manifest.5768e3aef31acebedc55d3af0edf2061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6bf0b0040f84aa87773f7dc2bf0fbd",
+    "url": "/Grade-Manager/index.html"
+  },
+  {
+    "revision": "99813466b1973fb38251",
+    "url": "/Grade-Manager/static/css/main.7be21890.chunk.css"
+  },
+  {
+    "revision": "b01316e7d2253877ff85",
+    "url": "/Grade-Manager/static/js/2.17b4d53b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Grade-Manager/static/js/2.17b4d53b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99813466b1973fb38251",
+    "url": "/Grade-Manager/static/js/main.9a131568.chunk.js"
+  },
+  {
+    "revision": "b1bb2c51945807f17184",
+    "url": "/Grade-Manager/static/js/runtime-main.f0911a4f.js"
+  }
+]);
